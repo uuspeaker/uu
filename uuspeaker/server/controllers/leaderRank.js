61321560f50e5c6af62af27110dca3841dd2c123
uuspeaker <63253294@qq.com>
2018-01-28 23:52:58 +0800

    优化排行榜使正常显示用户昵称，增加复盘页面

--- uuspeaker/server/controllers/leaderRank.js
+++ uuspeaker/server/controllers/leaderRank.js
@@ -13,11 +13,24 @@ module.exports = async ctx => {
     leaderIncreaseScore: await mysql("user_base_info").innerJoin('user_score_detail', 'user_base_info.user_id', 'user_score_detail.user_id').select('user_base_info.recommend_user as user_id', mysql.raw("count(user_base_info.recommend_user) as total_score")).where('meeting_date', '>=', firstDayOfMonth).groupBy('user_base_info.recommend_user').orderBy('total_score', 'desc').limit(limit).offset(offset),
   }
 
+  var allUserInfo = await mysql("user_base_info").select('user_id','nick_name')
 
   for (i = 0; i < ctx.state.data.leaderScore.length; i++) {
     ctx.state.data.leaderScore[i].rank = i + 1;
+    for (j = 0; j < allUserInfo.length; j++) {
+      if(ctx.state.data.leaderScore[i].user_id == allUserInfo[j].user_id){
+        ctx.state.data.leaderScore[i].nick_name = allUserInfo[j].nick_name
+        break;
+      }
+    }
   }
   for (i = 0; i < ctx.state.data.leaderIncreaseScore.length; i++) {
     ctx.state.data.leaderIncreaseScore[i].rank = i + 1;
+    for (j = 0; j < allUserInfo.length; j++) {
+      if (ctx.state.data.leaderIncreaseScore[i].user_id == allUserInfo[j].user_id) {
+        ctx.state.data.leaderIncreaseScore[i].nick_name = allUserInfo[j].nick_name
+        break;
+      }
+    }
   }
 }
--- uuspeaker/server/controllers/studyRank.js
+++ uuspeaker/server/controllers/studyRank.js
@@ -7,11 +7,17 @@ module.exports = async ctx => {
   var offset = 0
   var firstDayOfMonth = dateUtil.getFormatDate(new Date(), 'yyyyMM01')
   ctx.state.data = {
-    studyScore: await mysql("user_score_detail").select('user_id', mysql.raw("count(user_id) as total_score")).groupBy('user_id').having('total_score', '>=', 5)
+    studyScore: await mysql("user_base_info").innerJoin('user_score_detail', 'user_base_info.user_id', 'user_score_detail.user_id').select('user_base_info.nick_name', mysql.raw("count(user_base_info.user_id) as total_score")).groupBy('user_base_info.user_id')
       .orderBy('total_score', 'desc').limit(limit).offset(offset),
 
-    studyIncreaseScore: await mysql("user_score_detail").select('user_id', mysql.raw("count(user_id) as total_score")).where('meeting_date', '>=', firstDayOfMonth).groupBy('user_id').having('total_score', '>=', 5)
+    studyIncreaseScore: await mysql("user_base_info").innerJoin('user_score_detail', 'user_base_info.user_id', 'user_score_detail.user_id').select('user_base_info.nick_name', mysql.raw("count(user_base_info.user_id) as total_score")).where('user_score_detail.meeting_date', '>=', firstDayOfMonth).groupBy('user_base_info.user_id')
       .orderBy('total_score', 'desc').limit(limit).offset(offset),
+  }
 
+  for (i = 0; i < ctx.state.data.studyScore.length; i++) {
+    ctx.state.data.studyScore[i].rank = i + 1;
+  }
+  for (i = 0; i < ctx.state.data.studyIncreaseScore.length; i++) {
+    ctx.state.data.studyIncreaseScore[i].rank = i + 1;
   }
 }
