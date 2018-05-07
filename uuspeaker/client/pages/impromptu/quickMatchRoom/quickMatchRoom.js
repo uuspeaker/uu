b7f5f203e8f64459592517649721732527a06bed
tianhan <63253294@qq.com>
2018-05-07 15:40:53 +0800

    优化匹配界面

--- uuspeaker/client/pages/impromptu/quickMatchRoom/quickMatchRoom.js
+++ uuspeaker/client/pages/impromptu/quickMatchRoom/quickMatchRoom.js
@@ -44,7 +44,12 @@ Page({
     audioType: 1,
     speeches:{},
     speechStatus: 1,
-    waitSeconds:0
+    waitSeconds:0,
+
+    poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    name: '此时此刻',
+    author: '许巍',
+    src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
   },
 
   // 进入rtcroom页面
@@ -272,7 +277,7 @@ Page({
     console.log(options)
     userId = options.userId
     roomId = options.roomId
-    getRoomInfoId = setInterval(this.getRoomInfo,3000)
+    //getRoomInfoId = setInterval(this.getRoomInfo,3000)
     this.initUserInfo()
     this.initAudio()
     setInterval(this.wait,1000)
