dbb001e153d368cff5946057f445181e935ed55d
uuspeaker <63253294@qq.com>
2018-06-06 00:49:40 +0800

    优化界面展示

--- uuspeaker/server/config.js
+++ uuspeaker/server/config.js
@@ -1,5 +1,5 @@
 const CONF = {
-  userControl:1,
+  userControl:0,
 
   port: '5757',
   rootPathname: '',
@@ -61,7 +61,7 @@ const CONF = {
     // Bucket 名称
     fileBucket: 'uuspeaker',
     // 文件夹
-    uploadFolder: 'test',
+    uploadFolder: 'audio',
     maxSize: 20
   },
 
--- uuspeaker/server/controllers/userInfo/userControl.js
+++ uuspeaker/server/controllers/userInfo/userControl.js
@@ -4,8 +4,9 @@ const config = require('../../config')
 module.exports = {
 
   get: async ctx => {
-    //var data = await mysql('user_control')
-    ctx.state.data = config.userControl
+    var data = await mysql('user_control')
+    // ctx.state.data = config.userControl
+    ctx.state.data = data[0].flag
   },
 
 }
